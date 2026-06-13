d4bf48bb36dde4837441d211c38d4f89f8bbe3e3
Dhiego da Costa <dhiegov@protonmail.com>
2026-06-13 11:30:54 -0300

    feat(Bsp): definir interface Bsp

---
+++ pratica-3-timer-adc-potenciometro/Core/Inc/Bsp.h
@@ -0,0 +1,21 @@
+#ifndef BSP_H
+#define BSP_H
+
+/** @brief Inicializar a placa e periféricos. */
+void Bsp_Init(void);
+
+/** @brief Ativar interrupção no período definido. */
+void Bsp_StartTimerInterrupt(void);
+
+/** @brief Retorne se passou o tempo definido. */
+bool Bsp_GetTimerOverflowFlag(void);
+
+/** @brief Zere a flag que indica que passou o tempo definido. */
+void Bsp_ClearTimerFlag(void);
+
+/** @brief Retorne leitura do ADC por polling.
+ *
+ * Bloqueante. */
+int Bsp_ReadAdcPolling(void);
+
+#endif
